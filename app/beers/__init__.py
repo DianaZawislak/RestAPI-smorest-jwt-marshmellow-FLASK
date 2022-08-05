f7a753264b70e99205c96efdf40896c9979ab3b3
Diana Zawislak <diana.zawislak@icloud.com>
2022-08-04 22:15:36 -0400

    updated get post put and delete for beers and breweries

--- app/beers/__init__.py
+++ app/beers/__init__.py
@@ -1,15 +1,15 @@
 import os
-
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 import marshmallow
 import pandas as pd
 from flask.views import MethodView
-from flask_jwt_extended import jwt_required
-from flask_smorest import Blueprint, Page
+from flask_smorest import Blueprint, Page, abort
 from app.db.models import Brewery, Beer
 from app.db import db
 from marshmallow import Schema, fields, EXCLUDE
 from marshmallow_sqlalchemy import field_for
 from app.extensions import Schema, AutoSchema, SQLCursorPage
+from flask_jwt_extended import jwt_required
 
 beers = Blueprint('Beers and Breweries', __name__, url_prefix="/", description="Beers by brewery")
 
@@ -18,8 +18,9 @@ class BrewerySchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.String()
 
-    class Meta(AutoSchema.Meta):
-        table = Brewery.__table__
+    class Meta:
+        type_ = "Brewery"
+        strict = True
 
     def update(self, item, new_item):
         pass
@@ -42,27 +43,28 @@ class BreweryQueryArgsSchema(Schema):
     brewery_id = marshmallow.fields.Int()
 
 
-class BeerQueryArgsSchema(Schema):
-    beer_id = marshmallow.fields.Int()
+class BreweryNameUpdateSchema(Schema):
+    name = fields.Str()
 
 
-@beers.route('/breweries')
+class BeerNameUpdateSchema(Schema):
+    name = fields.Str()
+
+
+@beers.route('/brewery')
 class Breweries(MethodView):
 
     @beers.etag
-    @beers.arguments(BreweryQueryArgsSchema, location='query')
     @beers.response(200, BrewerySchema(many=True))
-    def get(self, args):
-        """List Countries"""
-        # ret = Brewery.query.all()
-        beer_id = args.pop('beer_id', None)
-        ret = Brewery.query.filter_by(**args)
-        if beer_id is not None:
-            ret = ret.join(Brewery.beers).filter(Beer.id == beer_id)
+    @beers.doc(description="Return ALL Breweries from database", summary="Finds all breweries")
+    def get(self):
+        """List Breweries"""
+        ret = Brewery.query.all()
+
         return ret
 
     @beers.etag
-    @beers.arguments(BrewerySchema)
+    @beers.arguments(BrewerySchema, location="json")
     @beers.response(201, BrewerySchema)
     @beers.doc(security=[{"bearerAuth": []}])
     @jwt_required()
@@ -75,52 +77,49 @@ class Breweries(MethodView):
         return item
 
 
-@beers.route('/breweries:id')
-class BreweryById(MethodView):
+@beers.route("/brewery/<string:brewery_id>")
+class BreweriesById(MethodView):
+    @beers.doc(description="Return Breweries based on ID", summary="Finds breweries by ID")
+    @beers.doc(security=[{"bearerAuth": []}])
+    @jwt_required()
+    @beers.response(200, BrewerySchema)
+    def get(self, brewery_id):
+        """Breweries by ID"""
+        brewery = Brewery.query.get_or_404(brewery_id)
+        return brewery
 
-    @beers.etag
-    @beers.response(201, BrewerySchema)
-    def get(self, item_id):
-        """Brewery by ID"""
-        return Brewery.query.get_or_404(item_id)
+    #@beers.arguments(BreweryNameUpdateSchema)
 
     @beers.etag
     @beers.arguments(BrewerySchema, location="json")
-    @beers.response(201, BrewerySchema)
-    @beers.doc(parameters=[{'name': 'If-Match', 'in': 'header', 'required': 'true'}])
-    @beers.doc(security=[{"bearerAuth": []}])
-    @jwt_required
+    @beers.response(200, BrewerySchema)
     def put(self, new_item, item_id):
-        """Update Brewery"""
+        """Update an existing member"""
         item = Brewery.query.get_or_404(item_id)
         beers.check_etag(item, BrewerySchema)
         BrewerySchema().update(item, new_item)
         db.session.add(item)
         db.session.commit()
-        return item
+        return item, {"message": "Brewery name updated"}, 200
 
-    @beers.etag
-    @beers.response(204)
-    @beers.doc(parameters=[{'name': 'If-Match', 'in': 'header', 'required': 'true'}])
-    @beers.doc(security=[{"bearerAuth": []}])
-    @jwt_required
-    def delete(self, item_id):
-        """Delete Brewery"""
-        item = Brewery.query.get_or_404(item_id)
-        beers.check_etag(item, BrewerySchema)
-        db.session.delete(item)
+    def delete(self, brewery_id):
+        brewery = Brewery.query.get_or_404(brewery_id)
+        db.session.delete(brewery)
         db.session.commit()
+        return {"message": "Brewery deleted"}, 200
 
 
-@beers.route('/beers')
+@beers.route("/beers")
 class Beers(MethodView):
 
     @beers.etag
-    @beers.arguments(BeerQueryArgsSchema, location='query')
     @beers.response(200, BeerSchema(many=True))
-    def get(self, args):
-        """List Beers"""
-        return Beer.query.filter_by(**args)
+    @beers.doc(description="Return ALL beers from database", summary="Finds all beers")
+    def get(self):
+        """List beers"""
+        ret = Beer.query.all()
+
+        return ret
 
     @beers.etag
     @beers.arguments(BeerSchema, location="json")
@@ -136,36 +135,30 @@ class Beers(MethodView):
         return item
 
 
-@beers.route('/beers:id')
-class BeerById(MethodView):
-
+@beers.route("/beers/<string:beer_id>")
+class BeersById(MethodView):
     @beers.etag
-    @beers.response(201, BeerSchema(many=True))
-    def get(self, item_id):
-        """Beers by ID"""
-        return Beers.query.get_or_404(item_id)
-
-    @beers.etag
-    @beers.arguments(BeerSchema, location="json")
-    @beers.response(201, BeerSchema)
-    @beers.doc(parameters=[{'name': 'If-Match', 'in': 'header', 'required': 'true'}])
+    @beers.doc(description="ReturnBeers based on ID", summary="Finds beers by ID")
     @beers.doc(security=[{"bearerAuth": []}])
-    @jwt_required
-    def put(self, update_data, beer_id):
-        """Update existing beer"""
-        item = Beer.get_by_id(beer_id)
-        item.update(update_data)
-        item.commit()
-        return item
+    @jwt_required()
+    @beers.response(200, BeerSchema)
+    def get(self, beer_id):
+        """Breweries by ID"""
+        beer = Beer.query.get_or_404(beer_id)
+        return beer
 
-    @beers.etag
-    @beers.response(204)
-    @beers.doc(parameters=[{'name': 'If-Match', 'in': 'header', 'required': 'true'}])
-    @beers.doc(security=[{"bearerAuth": []}])
-    @jwt_required
-    def delete(self, item_id):
-        """Delete Beers"""
-        item = Beers.query.get_or_404(item_id)
+    def put(self, new_item, item_id):
+        """Modify existing beer by ID"""
+        item = Beer.query.get_or_404(item_id)
         beers.check_etag(item, BeerSchema)
-        db.session.delete(item)
+        BeerSchema().update(item, new_item)
+        db.session.add(item)
         db.session.commit()
+        return item, {"message": "Beer updated"}, 200
+
+    def delete(self, beer_id):
+        """Delete ber"""
+        beer = Beer.query.get_or_404(beer_id)
+        db.session.delete(beer)
+        db.session.commit()
+        return {"message": "Beer deleted"}, 200
