c2d147a1650c927462dc60e0cad21a8696bf7884
Diana Zawislak <diana.zawislak@icloud.com>
2022-08-05 21:26:20 -0400

    token refresh and other user tests

---
+++ tests/test_option_you_pick_two.py
@@ -0,0 +1,90 @@
+"""user tests with token refresh"""
+import pytest
+
+
+@pytest.fixture()
+def created_user_jwts(client, created_user_details):
+    """fixture"""
+    username, password = created_user_details
+    response = client.post(
+        "/auth",
+        json={"username": username, "password": password},
+    )
+
+    return response.json["access_token"]
+
+
+@pytest.fixture()
+def created_user_details(client):
+    """fixture"""
+    username = "test_user"
+    password = "test_password"
+    client.post(
+        "/register",
+        json={"username": username, "password": password},
+    )
+
+    return username, password
+
+
+def test_refresh_token(client, created_user_jwts):
+    """testing refresh token"""
+    response = client.post(
+        "/refresh",
+        headers={"Authorization": f"Bearer {created_user_jwts[1]}"},
+    )
+
+    assert response.status_code == 200
+    assert response.json["access_token"]
+
+
+def test_register_user_already_exists(client):
+    """testing user already exists"""
+    username = "test_user"
+    client.post(
+        "/register",
+        json={"username": username, "password": "Test Password"},
+    )
+
+    response = client.post(
+        "/register",
+        json={"username": username, "password": "Test Password"},
+    )
+
+    assert response.status_code == 200
+
+
+
+def test_login_user_bad_password(client, created_user_details):
+    """testing wrong password"""
+    username, _ = created_user_details
+    response = client.post(
+        "/auth",
+        json={"username": username, "password": "bad_password"},
+    )
+
+    assert response.status_code == 401
+
+
+def test_login_user_bad_username(client, created_user_details):
+    """testing wrong user name"""
+    _, password = created_user_details
+    response = client.post(
+        "/auth",
+        json={"username": "bad_username", "password": password},
+    )
+
+    assert response.status_code == 401
+
+
+def test_get_user_details(client, created_user_details):
+    """testing get user details"""
+    response = client.get(
+        "/user_info/1",  # assume user id is 1
+    )
+
+    assert response.status_code == 200
+    assert response.json == {
+        "id": 1,
+        "username": created_user_details[0],
+    }
