0859c7c6f2032087ddeaac6d2c5e44f9f123f305
Diana Zawislak <diana.zawislak@icloud.com>
2022-08-05 21:25:50 -0400

    geography tests

--- tests/test_geography_project2.py
+++ tests/test_geography_project2.py
@@ -9,67 +9,47 @@ from app.db.models import City, Country, User, Beer, Brewery
 # pylint: disable=consider-using-f-string, no-member, undefined-variable
 # pylint: disable=wrong-import-order, no-value-for-parameter, unused-argument
 
-def test_create_brewery(application):
-    """this tests create the brewery method"""
-    with application.app_context():
-        brewery = Brewery(name="18th Street Brewery")
-        db.session.add(brewery)
-        db.session.commit()
 
-        assert brewery.name == "18th Street Brewery"
+def test_delete_city(client, created_city_id):
+    """this tests city delete method"""
+    response = client.delete(
+        f"/item/{created_city_id}",
+    )
 
-def test_create_beer(application):
-    """this tests create the beer method"""
-    with application.app_context():
-        brewery = Brewery(name="18th Street Brewery")
-        beer = Beer(name="Devil's Cup", brewery_id=brewery.id)
-        db.session.add(beer)
-        db.session.commit()
-
-        assert beer.name == "Devil's Cup"
+    assert response.status_code == 200
+    assert response.json["message"] == "City deleted"
 
 
-def test_brewery_post(client, create_user):
-    # pylint: disable=unused-argument
-    """this tests country post method"""
-    with client.application.app_context():
-        access_token = create_token()
+def test_get_city_not_found(client):
+    """this tests city not found"""
+    response = client.get(
+        "/city/1999",
+    )
 
-    data = {"brewery": "18th Street Brewery"}
-
-    headers = {
-        'Authorization': 'Bearer {}'.format(access_token)
-    }
-    response = client.post('/beer/breweries', json=data, headers=headers)
-    response_data = response.get_json()
-    assert response.status_code == 201
-    assert response_data["brewery"] == "18th Street Brewery"
+    assert response.status_code == 404
+    assert response.json == {"code": 404, "status": "Not Found"}
 
 
+def test_post_country_without_admin(client, jwt, created_country_id):
+    """this tests post country without authorization"""
+    response = client.post(
+        f"/countries/{created_country_id}",
+        headers={"Authorization": f"Bearer {jwt}"},
+    )
 
-def test_beer_post(client, create_user):
-    # pylint: disable=unused-argument
-    """this tests beer post method"""
-    with client.application.app_context():
-        access_token = create_token()
+    assert response.status_code == 401
+    assert response.json["message"] == "Missing Authorization Header"
 
-    data = {"brewery": "United States"}
 
-    headers = {
-        'Authorization': 'Bearer {}'.format(access_token)
-    }
-    response = client.post('/beer/breweries', json=data, headers=headers)
-    response_data = response.get_json()
-    brewery_id = response_data['brewery_id']
-    pprint(brewery_id)
+def test_post_city_without_admin(client, jwt, created_city_id):
+    """this tests post city without authorization"""
+    response = client.post(
+        f"/cities/{created_city_id}",
+        headers={"Authorization": f"Bearer {jwt}"},
+    )
 
-    data = {"brewery_id": brewery_id, "name": "Devil's Cup"}
-
-    response = client.post('/beer/breweries', json=data, headers=headers)
-    assert response.status_code == 201
-    response_data = response.get_json()
-    assert response_data["id"] == 1
-    assert response_data["name"] == "Devil's Cup"
+    assert response.status_code == 401
+    assert response.json["message"] == "Missing Authorization Header"
 
 
 def create_token():
@@ -77,23 +57,3 @@ def create_token():
     user = User.query.first()
     access_token = create_access_token(user.username)
     return access_token
-
-def test_countries_route():
-    """Testing the routes to list records"""
-    response = app.test_client().get('/countries')
-    assert response.status_code == 304
-
-def test_city_route():
-    """Testing the routes to list records"""
-    response = app.test_client().get('/cities')
-    assert response.status_code == 304
-
-def test_brewery_route():
-    """Testing the routes to list records"""
-    response = app.test_client().get('/breweries')
-    assert response.status_code == 304
-
-def test_beer_route():
-    """Testing the routes to list records"""
-    response = app.test_client().get('/beers')
-    assert response.status_code == 304
